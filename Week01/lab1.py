0b09699debd77fc2c66616eb07730f17a648b14f
rohan437-3104 <rohan.davis@georgebrown.ca>
2026-01-16 23:02:31 -0500

    add q1, q2, q5

---
+++ Week01/lab1.py
@@ -0,0 +1,15 @@
+# Sample Coding Question 01 Week 01
+# Rohan Davis
+
+# Question 2: Defining an array (List)
+my_array = [1, 4, 7, 9]
+
+# Question 3:
+
+
+# Question 4:
+
+# Question 5:
+userAge = int(input("Enter your age: "))
+userAge = userAge + 22
+print("Now Showing the shop items filtered age:", userAge)
---
+++ Week01/q5.py
@@ -0,0 +1,3 @@
+userAge = int(input("Enter your age: "))
+userAge = userAge + 22
+print("Now Showing the shop items filtered age:", userAge)
